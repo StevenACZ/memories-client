3662d33f3d410062202f6cbdaca47137cfc9c9b8
stevenacz <scoailazaa@gmail.com>
2021-08-16 12:33:50 -0500

    styles added

--- src/components/Posts/Posts.jsx
+++ src/components/Posts/Posts.jsx
@@ -1,10 +1,15 @@
 // React
 import React from 'react';
 
+// Styles
+import useStyles from './Styles';
+
 // Components
 import Post from './Post/Post.jsx';
 
 function Posts() {
+	const classes = useStyles();
+
 	return (
 		<div>
 			<h1>Posts</h1>
