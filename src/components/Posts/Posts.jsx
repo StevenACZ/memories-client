00991454ee4a6a64e08e8d4a3532dc1144e46673
stevenacz <scoailazaa@gmail.com>
2021-08-16 12:18:39 -0500

    Improved

---
+++ src/components/Posts/Posts.jsx
@@ -0,0 +1,16 @@
+// React
+import React from 'react';
+
+// Components
+import Post from './Post/Post.jsx';
+
+function Posts() {
+	return (
+		<div>
+			<h1>Posts</h1>
+			<Post />
+		</div>
+	);
+}
+
+export default Posts;
