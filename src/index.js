6b625e757db15c3bb2707fa75dd83802aeb1dace
stevenacz <scoailazaa@gmail.com>
2021-08-16 13:12:36 -0500

    redux added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
+// React
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// Redux
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+
+// Redux - Reducers
+import reducers from './reducers';
+
+// Components
 import App from './App.jsx';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducers, compose(applyMiddleware(thunk)));
+
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById('root')
+);
